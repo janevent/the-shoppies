b3810347fb59d1b83fc3b7f253effc8ed21441ea
janeventer <janeanneventer@gmail.com>
2021-01-06 15:48:43 -0800

    display the movie, set state, and create nominate, and denominate functions

--- src/DisplayMovie.js
+++ src/DisplayMovie.js
@@ -2,27 +2,43 @@ import React from 'react';
 
 export default class DisplayMovie extends React.Component{
     
+    constructor(props){
+        super(props)
+        this.state = {
+            title: props.title,
+            year: props.year,
+            nominated: false
+        }
+    }
+
     clickNominate = (event) => {
         this.setState({
             nominated: true
         })
+        //add to nominated list
     }
 
-    
+    clickDenominated = () => {
+        this.setState({
+            nominated: false
+        })
+        //take away from nominated list
+    }
+
+
 
     componentDidMount(){
         console.log("DisplayMovie did mount");
         console.log(this.props)
     }
 
-    render(){
-        const {title, year} = this.props 
-        console.log(title, year)
+    render(){ 
+        
         return(
             <div className="DisplayMovie">
                 <h1>Display Movie</h1>
-                <h2>{title}</h2>
-                <p>{year}</p>
+                <h2>{this.state.title}</h2>
+                <p>{this.state.year}</p>
             </div>
         )
     }
