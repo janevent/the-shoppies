26604017761228ed73c5ec73d9e41f11b1fe30dd
janeventer <janeanneventer@gmail.com>
2021-01-10 14:45:41 -0800

    display nominations if there are 5 nominations

--- src/Movies.js
+++ src/Movies.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import DisplayNominations from'./DisplayNominations';
+import { connect } from 'react-redux';
 //import DisplayMovies from './DisplayMovies';
 
 function Movies() {
     return (
         <div className="Movies">
             <SearchBar />
-            
+            {this.props.nominations.length === 5 ?
+                <DisplayNominations /> :
+                ""
+            }
         </div>
     )
 }
 
-export default Movies
+const mapStateToProps = (state) => {
+    return {
+        nominations: state.nominations
+    }
+}
+
+export default connect(mapStateToProps)(Movies)
