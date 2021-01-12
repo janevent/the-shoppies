2b0dff4112fd728e4c19a74a33504b19c73dfc0d
janeventer <janeanneventer@gmail.com>
2021-01-12 14:25:00 -0800

    add searchedMovies to the reducer"

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import nominationsReducer from './reducers/nominationsReducer';
+import setSearchedMovies from './reducers/setSearchedMovies';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
-    nominations: nominationsReducer
+    nominations: nominationsReducer,
+    searchedMovies: setSearchedMovies
 });
 
 const store = createStore(reducer, applyMiddleware(thunk));
