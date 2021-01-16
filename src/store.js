5c763a1c9e91b1105b03389d96aff2313fe53a49
janeventer <janeanneventer@gmail.com>
2021-01-16 13:52:20 -0800

    add moviesReviewsReducer to combineReducers

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import nominationsReducer from './reducers/nominationsReducer';
 import searchedMoviesReducer from './reducers/searchedMoviesReducer';
+import moviesReviewsReducer from './reducers/moviesReviewsReducer';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
     nominations: nominationsReducer,
-    searchedMovies: searchedMoviesReducer
+    searchedMovies: searchedMoviesReducer,
+    reviews: moviesReviewsReducer
 });
 
 const store = createStore(reducer, applyMiddleware(thunk));
