8ceebebcd306a5c87233031795d49f48035a9b09
janeventer <janeanneventer@gmail.com>
2021-01-16 13:50:09 -0800

    use routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Movies from './Movies';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -7,7 +8,10 @@ function App() {
       <header className="App-header">
         The Shoppies
       </header>
-      <Movies/>
+      <Switch>
+        
+        <Route path="/" component={Movies} />
+      </Switch >
     </div>
   );
 }
