f904953de74b03479dfe5db8cb0e9b39057fb023
janeventer <janeanneventer@gmail.com>
2021-01-17 10:38:55 -0800

    display nominations on nomination page

--- src/NominationsPage.js
+++ src/NominationsPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DisplayNomination from './DisplayNomination';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
  
@@ -6,11 +7,17 @@ class NominationsPage extends React.Component {
     
 
     render(){
+        const allNominations = this.props.nominations.map((nom) => {
+            return (
+                <DisplayNomination nomination={nom} />
+            )
+
+        })
         return (
             <div className="NominationsPage">
                 <div className="links"><Link to="/">Search Movies</Link>  <Link to="/reviews">Reviews</Link></div>
                 {this.props.nominations.length > 0 ?
-                <p>nominations</p> :
+                <div>{allNominations}</div> :
                 <p>There are no nominations yet</p>
                 }
             </div>
